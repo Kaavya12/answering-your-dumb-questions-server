ce07802138d84568d37c65f0ee9fb45d2f95ee14
Kaavya12 <kaavyamaha12@gmail.com>
2021-06-07 21:26:28 +0530

    Login backend setup

--- src/controllers/AuthenticationController.js
+++ src/controllers/AuthenticationController.js
@@ -1,8 +1,11 @@
 const { User } = require('../models')
+const bcrypt = require('bcrypt');
 
 module.exports = {
   async register (req, res) {
     try {
+      const salt = await bcrypt.genSalt(10);
+      req.body.password = await bcrypt.hash(req.body.password, salt);
       const user = await User.create(req.body)
       res.send({
         message: 'User successfully created',
@@ -18,6 +21,20 @@ module.exports = {
       res.status(400).send({
         error: errorMessage
       })
+    }
+  },
+  async login (req, res) {
+    const body = req.body;
+    const user = await User.findOne({where: { username: body.username }});
+    if (user) {
+      const validPassword = await bcrypt.compare(body.password, user.password);
+      if (validPassword) {
+        res.status(200).json({ message: "Valid password" });
+      } else {
+        res.status(400).json({ error: "Invalid Password" });
+      }
+    } else {
+      res.status(401).json({ error: "User does not exist" });
     }
   }
 }
